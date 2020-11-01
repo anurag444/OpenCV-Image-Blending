635d638165961e506134bcbc5ac829322a030265
anurag444 <37261718+anurag444@users.noreply.github.com>
2020-11-02 01:18:21 +0530

    Python File added

---
+++ image_blending_using_pyramids.py
@@ -0,0 +1,67 @@
+import cv2
+import numpy as np
+
+apple = cv2.imread('apple.jpg')
+orange = cv2.imread('orange.jpg')
+
+cv2.imshow('apple', apple)
+cv2.imshow('orange', orange)
+
+# Blending images normally by cutting both in half
+apple_orange = np.hstack((apple[:,:256], orange[:, 256:]))
+cv2.imshow('apple_orange', apple_orange)
+
+# Blending using Pyramids
+
+#Step 1 - Generating Gaussian pyramid for apple and orange
+apple_copy = apple.copy()
+gp_apple = [apple_copy]
+
+for i in range(6):
+    apple_copy = cv2.pyrDown(apple_copy)
+    gp_apple.append(apple_copy)
+
+orange_copy = orange.copy()
+gp_orange = [orange_copy]
+
+for i in range(6):
+    orange_copy = cv2.pyrDown(orange_copy)
+    gp_orange.append(orange_copy)
+
+# Step 2 - Generating Laplacian pyramid for apple and orange
+apple_copy = gp_apple[5]
+lp_apple = [apple_copy]
+
+for i in range(5, 0 , -1):
+    gaussian_expanded = cv2.pyrUp(gp_apple[i])
+    laplacian = cv2.subtract(gp_apple[i-1], gaussian_expanded)
+    lp_apple.append(laplacian)
+
+orange_copy = gp_orange[5]
+lp_orange = [orange_copy]
+
+for i in range(5, 0 , -1):
+    gaussian_expanded = cv2.pyrUp(gp_orange[i])
+    laplacian = cv2.subtract(gp_orange[i-1], gaussian_expanded)
+    lp_orange.append(laplacian)
+
+#Step 3 - Join halves of two images
+apple_orange_pyramid = []
+n=0
+for apple_lap, orange_lap in zip(lp_apple, lp_orange):
+    n += 1
+    cols, rows, ch= apple_lap.shape
+    laplacian = np.hstack((apple_lap[:, 0:int(cols/2)], orange_lap[:, int(cols/2):]))
+    apple_orange_pyramid.append(laplacian)
+
+#Step 4 - Reconstruction
+apple_orange_reconstruct = apple_orange_pyramid[0]
+for i in range(1,6):
+    apple_orange_reconstruct = cv2.pyrUp(apple_orange_reconstruct)
+    apple_orange_reconstruct = cv2.add(apple_orange_pyramid[i], apple_orange_reconstruct)
+
+cv2.imshow('apple_orange_reconstruct', apple_orange_reconstruct)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
